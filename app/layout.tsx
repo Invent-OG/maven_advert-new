2d692ed47ac64ce74aae658135b560379f07d41e
gunavarma <gunavarma0000@gmail.com>
2026-01-22 22:17:45 +0530

    GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,3 @@
-// "use client";
-
-// import "./globals.css";
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-// import LenisProvider from "./providers/LenisProvider";
-// import { usePathname } from "next/navigation";
-// import { Toaster } from "react-hot-toast";
-// import QueryProvider from "./providers/QueryProvider";
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const pathname = usePathname();
-//   const isAdminRoute = pathname.startsWith("/admin");
-
-//   return (
-//     <html lang="en">
-//       <body className="antialiased" suppressHydrationWarning={true}>
-//         <QueryProvider>
-//           {isAdminRoute ? (
-//             <main>{children}</main>
-//           ) : (
-//             <LenisProvider>
-//               {!isAdminRoute && <Navbar />}
-//               <main>{children}</main>
-//               {!isAdminRoute && <Footer />}
-//               {/* ✅ Toast Provider */}
-//               <Toaster
-//                 position="top-center"
-//                 containerStyle={{ zIndex: 9999 }}
-//                 toastOptions={{
-//                   duration: 4000,
-//                   style: {
-//                     borderRadius: "10px",
-//                     background: "#333",
-//                     color: "#fff",
-//                   },
-//                   success: {
-//                     iconTheme: { primary: "#22c55e", secondary: "#fff" },
-//                   },
-//                   error: {
-//                     iconTheme: { primary: "#ef4444", secondary: "#fff" },
-//                   },
-//                 }}
-//               />
-//             </LenisProvider>
-//           )}
-//         </QueryProvider>
-//       </body>
-//     </html>
-//   );
-// }
 "use client";
 
 import "./globals.css";
@@ -83,6 +28,21 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
         suppressHydrationWarning={true}
       >
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PXM9CLGB');`}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PXM9CLGB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Script
           src="https://cdn-in.pagesense.io/js/mavenadvert/da6dbad7d7624e8db32cbed1b852ee0c.js"
           strategy="afterInteractive"
