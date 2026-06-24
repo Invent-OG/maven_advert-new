a92f3ded034319cbb55002f4afc0cb49e3e02735
gunavarma <gunavarma0000@gmail.com>
2026-06-24 17:02:44 +0530

    refactor: migrate manual GTM script injection to @next/third-parties GoogleTagManager component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+// app/layout.tsx
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import ClientLayout from "@/components/ClientLayout";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,32 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PXM9CLGB');`,
-          }}
-        />
-      </head>
-      <body
-        className={`${inter.className} antialiased`}
-        suppressHydrationWarning={true}
-      >
-        {/* GTM Noscript as absolute first child of body */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PXM9CLGB"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        
-        {/* Rest of components delegated to Client component wrapper */}
+      <body className={inter.className}>
+        <GoogleTagManager gtmId="GTM-PXM9CLGB" />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
